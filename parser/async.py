6cbd9efcaa3ea4d615409da3eed01487443c1437
Yegor <gare577@gmail.com>
2022-02-19 23:47:36 +0300

    Tried to make async requests
    Small perfomance gain. Needs to redo.

--- parser/async.py
+++ parser/async.py
@@ -1,47 +1,77 @@
 import asyncio
 import aiohttp
 from time import time
-from main import search_for_hrefs, get_main_url
+from statistics import mean
+from random import choice
+from main import search_for_hrefs, get_main_url, write_report, USER_AGENTS
 
 
-links_to_test = [
-    "https://dvmn.org/contacts/",
-    "https://dvmn.org/encyclopedia/",
-    "https://dvmn.org/encyclopedia/about-chatbots/chat-bot-design/",
-    "https://dvmn.org/encyclopedia/about-chatbots/long-polling/",
-    "https://dvmn.org/encyclopedia/about-chatbots/ptbot-tutorial/",
-    "https://dvmn.org/encyclopedia/about-chatbots/webhook/",
-    "https://dvmn.org/encyclopedia/api-docs/yandex-geocoder-api/",
-    "https://dvmn.org/encyclopedia/async_python/coroutine_bomb/",
-    "https://dvmn.org/encyclopedia/async_python/coroutines/",
-    "https://dvmn.org/encyclopedia/async_python/how-to-stop-coroutine/",
-    "https://dvmn.org/encyclopedia/async_python/split_corutine/",
-    "https://dvmn.org/encyclopedia/async_python/why_async/",
-]
+async def request_and_scan_page(session: aiohttp.ClientSession, url: str):
+    headers = {
+        "User-Agent": choice(USER_AGENTS)
+    }
+    async with session.get(url, headers=headers) as response:
+        response.raise_for_status()
+        print("scanning:", url)
+        page = await response.text()
+
+    main_url = get_main_url(url)
+    clean_links, _ = search_for_hrefs(main_url, page)
+    return list(set(clean_links))
 
 
-async def run_for_pages():
-    links = set()
+async def run_for_pages(first_url: str):
+    pages_to_scan = asyncio.Queue()
+    pages_to_scan.put_nowait(first_url)
+
+    pages_scanned = set()
+    pages_found = set()
+
+    times = []
+    func_time = time()
 
     async with aiohttp.ClientSession() as session:
-        for url in links_to_test:
-            async with session.get(url) as response:
-                print("scanning:", url)
-                page = await response.text()
+        while not pages_to_scan.empty():
+            scan_time = time()
+            url = await pages_to_scan.get()
 
-            main_url = get_main_url(url)
-            clean_links, dirt_links = search_for_hrefs(main_url, page)
-            links.update(clean_links)
+            if url in pages_scanned:
+                pages_to_scan.task_done()
+                continue
 
-    return links
+            links = await request_and_scan_page(session, url)
+
+            pages_found.update(links)
+            pages_scanned.add(url)
+            unique_links = set(links) - pages_scanned
+            for item in unique_links:
+                pages_to_scan.put_nowait(item)
+
+            times.append(time() - scan_time)
+            pages_to_scan.task_done()
+
+            if len(pages_scanned) % 10 == 0:
+                print(f"scanned: {len(pages_scanned)}, left to scan: {pages_to_scan.qsize()}")
+
+            if len(pages_found) > 500:
+                print("\nreached pages limit.")
+                break
+
+    print(f"\ndone by {time() - func_time:.2f} secs.\n")
+    print(f"scanned: {len(pages_scanned)}")
+    print(f"found: {len(pages_found)}\n")
+    print(f"mean time per page: {mean(times):.2f}")
+    print(f"max time per page: {max(times):.2f}")
+    print(f"min time per page: {min(times):.2f}")
+
+    return pages_scanned, sorted(pages_found)
 
 
 if __name__ == "__main__":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-    start = time()
+    url = "https://gljewelry.com/about/"
+    url = "https://www.google.com/"
 
-    links = asyncio.run(run_for_pages())
-
-    print(time() - start)
-
+    scanned, found = asyncio.run(run_for_pages(url))
+    write_report(url, len(scanned), found, "async")
