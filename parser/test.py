caabfc0acf7b74815866e5a2a77df83c9cac449f
Yegor <gare577@gmail.com>
2022-03-25 17:39:31 +0300

    Refactored tests

--- parser/test.py
+++ parser/test.py
@@ -1,7 +1,6 @@
 import json
 import asyncio
 from os import listdir
-from statistics import mean
 
 from main import scan_page, write_report, run_for_pages, examples
 from asynchro import run_for_pages as run_async
@@ -52,16 +51,18 @@ def test_scan_page(samples: list):
         links, dirt_links = scan_page(url)
 
         if len(links) != sample["found"]:
-            write_report(url, "t_clean",
-                         scanned=1,
-                         found=len(links),
-                         endpoints=sorted(links),
-                         )
-            write_report(url, "t_dirt",
-                         scanned=1,
-                         found=len(dirt_links),
-                         endpoints=sorted(dirt_links)
-                         )
+            write_report(
+                url, "t_clean",
+                scanned=1,
+                found=len(links),
+                endpoints=sorted(links),
+            )
+            write_report(
+                url, "t_dirt",
+                scanned=1,
+                found=len(dirt_links),
+                endpoints=sorted(dirt_links)
+            )
             raise FailedTest(
                 f"url: {url} expected: {sample['found']} "
                 f"vs scanned: {len(links)}")
@@ -69,48 +70,46 @@ def test_scan_page(samples: list):
         print("successfully tested.")
 
 
-def test_report(url: str, postfix: str, times: list,
+def test_report(url: str, postfix: str, times: dict,
                 scanned: list, found: list):
-    total_time = times.pop()
     write_report(
         url, postfix,
-        total_time=round(total_time, 2),
-        mean_time=round(mean(times), 2),
         scanned=len(scanned),
         found=len(found),
+        times=times,
         endpoints=found,
     )
 
 
 def test_sync(url: str):
-    print("scanning synchronously.\n")
+    print("scanning synchronously.")
 
-    print(f"scanning with time limit:", url)
+    print("\nscanning with time limit.\n")
     times, scanned, found = run_for_pages(url, time_limit=5)
     test_report(url, "t_time_sync", times, scanned, found)
 
-    print(f"scanning with scanned limit:", url)
+    print("\nscanning with scanned limit.\n")
     times, scanned, found = run_for_pages(url, scanned_limit=15)
     test_report(url, "t_scanned_sync", times, scanned, found)
 
-    print(f"scanning with found limit:", url)
+    print("\nscanning with found limit.\n")
     times, scanned, found = run_for_pages(url, found_limit=250)
     test_report(url, "t_found_sync", times, scanned, found)
 
 
 def test_async(url: str):
-    print("scanning asynchronously.\n")
+    print("scanning asynchronously.")
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-    print(f"scanning with time limit:", url)
+    print("\nscanning with time limit.\n")
     times, scanned, found = asyncio.run(run_async(url, time_limit=5))
     test_report(url, "t_time_async", times, scanned, found)
 
-    print(f"scanning with scanned limit:", url)
+    print("\nscanning with scanned limit.\n")
     times, scanned, found = asyncio.run(run_async(url, scanned_limit=15))
     test_report(url, "t_scanned_async", times, scanned, found)
 
-    print(f"scanning with found limit:", url)
+    print("\nscanning with found limit.\n")
     times, scanned, found = asyncio.run(run_async(url, found_limit=250))
     test_report(url, "t_found_async", times, scanned, found)
 
